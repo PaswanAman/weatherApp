6bf64f68c63d8a23e97e7a9e57a89f8315013b86
PaswanAman <145580828+PaswanAman@users.noreply.github.com>
2024-05-08 22:55:38 +0545

    weatherapp

---
+++ src/api.js
@@ -0,0 +1,17 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ac0508ccdmsh7fb0b525361ca2ep189f65jsn9bf2e90d25ec',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b38d4fdb4dd57ee9d2b4c028275b3204";
+
+
+
+ 
+
